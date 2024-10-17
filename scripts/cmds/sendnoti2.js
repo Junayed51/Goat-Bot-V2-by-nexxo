dfe2795f0745bc77d8036ad2501a273d6c0fb129
Junayed51 <jabed913651@gmail.com>
2024-10-17 18:54:21 +0600

    Update sendnoti2.js

--- scripts/cmds/sendnoti2.js
+++ scripts/cmds/sendnoti2.js
@@ -4,7 +4,7 @@ const { resolve } = require("path");
 
 module.exports = {
         config: {
-                name: "sendnoti",
+                name: "sendnoti2",
                 version: "1.4",
                 author: "cliff",
                 countDown: 5,
